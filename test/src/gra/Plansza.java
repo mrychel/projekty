ec41e05cd2ddf0f6747d4bfd0973f093dd44e289
Mirek Rychel <mirek.rychel@gmail.com>
2016-08-22 17:42:34 +0200

    22 sierpnia 2016

--- test/src/gra/Plansza.java
+++ test/src/gra/Plansza.java
@@ -49,23 +49,31 @@ public class Plansza {
             	plansza[x][y] = new Pole(0, null);		
 		
     	// true = biale
-    	plansza[1][0].ustawPionek(true);
-    	plansza[3][0].ustawPionek(true);
-    	plansza[5][0].ustawPionek(true);
-    	plansza[7][0].ustawPionek(true);
-    	plansza[0][1].ustawPionek(true);
-    	plansza[2][1].ustawPionek(true);
-    	plansza[4][1].ustawPionek(true);
-    	plansza[6][1].ustawPionek(true);
+    	plansza[1][0].ustawPionek(false);
+    	plansza[3][0].ustawPionek(false);
+    	plansza[5][0].ustawPionek(false);
+    	plansza[7][0].ustawPionek(false);
+    	plansza[0][1].ustawPionek(false);
+    	plansza[2][1].ustawPionek(false);
+    	plansza[4][1].ustawPionek(false);
+    	plansza[6][1].ustawPionek(false);
+    	plansza[1][2].ustawPionek(false);
+    	plansza[3][2].ustawPionek(false);
+    	plansza[5][2].ustawPionek(false);
+    	plansza[7][2].ustawPionek(false);
     	// false = czarne
-    	plansza[1][6].ustawPionek(false);
-    	plansza[3][6].ustawPionek(false);
-    	plansza[5][6].ustawPionek(false);
-    	plansza[7][6].ustawPionek(false);
-    	plansza[0][7].ustawPionek(false);
-    	plansza[2][7].ustawPionek(false);
-    	plansza[4][7].ustawPionek(false);
-    	plansza[6][7].ustawPionek(false);
+    	plansza[1][6].ustawPionek(true);
+    	plansza[3][6].ustawPionek(true);
+    	plansza[5][6].ustawPionek(true);
+    	plansza[7][6].ustawPionek(true);
+    	plansza[0][7].ustawPionek(true);
+    	plansza[2][7].ustawPionek(true);
+    	plansza[4][7].ustawPionek(true);
+    	plansza[6][7].ustawPionek(true);
+    	plansza[0][5].ustawPionek(true);
+    	plansza[2][5].ustawPionek(true);
+    	plansza[4][5].ustawPionek(true);
+    	plansza[6][5].ustawPionek(true);
     }   
 	
 	public class Pole {
